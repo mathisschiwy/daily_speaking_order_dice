316272d79f2d1abee9dfe0548c042e9bcaed0c77
mathisschiwy <mathis.schiwy@greenbone.net>
2023-06-20 11:33:55 +0200

    Some changes due to better readabilty

--- project_order_dice/main.py
+++ project_order_dice/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import functions
+from functions import names
 print("-------------------------Daily Speaking Order Dice--------------------- ")
 print("Below you can enter the names of the team members who are not present")
 print("If all members are present simply press return")
 
 # Function to exclude the names of people who are not present.
-names = ["Hannes", "Jessica", "Henning", "Mathis", "Sarah", "Steffen"]
 functions.enter_names(names)
 
 # Generate the speaking order.
