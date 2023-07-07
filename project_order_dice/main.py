5c18b03249062c0f1c05343c5530a52a4f526289
mathisschiwy <mathis.schiwy@greenbone.net>
2023-07-07 13:38:39 +0200

    Added a few comments

--- project_order_dice/main.py
+++ project_order_dice/main.py
@@ -5,7 +5,7 @@ print("-------------------------Daily Speaking Order Dice--------------------- "
 print("Below you can enter the names of the team members who are not present")
 print("If all members are present simply press return")
 data = read_away_yaml()
-# Function to exclude the names of people who are not present.
+# Function to build the list of participating names
 filtered_names = functions.build_todays_participants(data)
 
 # Function to manually exclude people who are not available at the current day
